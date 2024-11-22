f9afdab773277cae51be381d99eb7143c53eeb9a
Gomdadi <35wnsgh@naver.com>
2024-11-23 02:07:02 +0900

    feat: Restaurant 조회 Service 구현 RestaurantRepository, RestaurantImageRepository 메서드 호출 후 dto로 변환

--- UOSense-Backend/src/main/java/UOSense/UOSense_Backend/service/RestaurantService.java
+++ UOSense-Backend/src/main/java/UOSense/UOSense_Backend/service/RestaurantService.java
@@ -1,4 +1,17 @@
 package UOSense.UOSense_Backend.service;
 
+import UOSense.UOSense_Backend.common.DoorType;
+import UOSense.UOSense_Backend.dto.RestaurantListResponse;
+import UOSense.UOSense_Backend.dto.RestaurantResponse;
+import UOSense.UOSense_Backend.entity.Restaurant;
+
+import java.util.List;
+
 public interface RestaurantService {
+    public List<RestaurantListResponse> getAllRestaurants();
+    public List<RestaurantListResponse> getRestaurantsByFilter(DoorType doorType, Restaurant.Category category);
+    public List<RestaurantListResponse> getRestaurantsByCategory(Restaurant.Category category);
+    public List<RestaurantListResponse> getRestaurantsByDoorType(DoorType doorType);
+    public RestaurantResponse getRestaurantById(int RestaurantId);
+
 }
--- UOSense-Backend/src/main/java/UOSense/UOSense_Backend/service/RestaurantServiceImpl.java
+++ UOSense-Backend/src/main/java/UOSense/UOSense_Backend/service/RestaurantServiceImpl.java
@@ -1,12 +1,84 @@
 package UOSense.UOSense_Backend.service;
 
+import UOSense.UOSense_Backend.common.DoorType;
+import UOSense.UOSense_Backend.dto.RestaurantListResponse;
+import UOSense.UOSense_Backend.dto.RestaurantResponse;
+import UOSense.UOSense_Backend.entity.Restaurant;
+import UOSense.UOSense_Backend.repository.BusinessDayRepository;
+import UOSense.UOSense_Backend.repository.RestaurantImageRepository;
 import UOSense.UOSense_Backend.repository.RestaurantRepository;
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Service;
 
+import java.util.ArrayList;
+import java.util.List;
+import java.util.NoSuchElementException;
+import java.util.Optional;
+
 @Service
 @RequiredArgsConstructor
 public class RestaurantServiceImpl implements RestaurantService{
-
     private final RestaurantRepository restaurantRepository;
+    private final BusinessDayRepository businessDayRepository;
+    private final RestaurantImageRepository restaurantImageRepository;
+
+    @Override
+    public List<RestaurantListResponse> getAllRestaurants() {
+        List<Restaurant> restaurants = restaurantRepository.findAll();
+        if (restaurants.isEmpty()) throw new NoSuchElementException("식당 리스트가 존재하지 않습니다.");
+        List<RestaurantListResponse> responseList = restaurants.stream()
+                .map(restaurant -> {
+                    String imageUrl = restaurantImageRepository.findFirstImageUrlByRestaurantOrderByIdAsc(restaurant);
+                    return RestaurantListResponse.from(restaurant, imageUrl);
+                })
+                .toList();
+        return responseList;
+    }
+
+    @Override
+    public List<RestaurantListResponse> getRestaurantsByFilter(DoorType doorType, Restaurant.Category category) {
+        List<Restaurant> restaurants = restaurantRepository.findByDoorTypeAndCategory(doorType, category);
+        if (restaurants.isEmpty()) throw new NoSuchElementException("해당 식당 리스트가 존재하지 않습니다.");
+        List<RestaurantListResponse> responseList = restaurants.stream()
+                .map(restaurant -> {
+                    String imageUrl = restaurantImageRepository.findFirstImageUrlByRestaurantOrderByIdAsc(restaurant);
+                    return RestaurantListResponse.from(restaurant, imageUrl);
+                })
+                .toList();
+        return responseList;
+    }
+
+    @Override
+    public List<RestaurantListResponse> getRestaurantsByCategory(Restaurant.Category category) {
+        List<Restaurant> restaurants = restaurantRepository.findByCategory(category);
+        if (restaurants.isEmpty()) throw new NoSuchElementException("해당 식당 리스트가 존재하지 않습니다.");
+        List<RestaurantListResponse> responseList = restaurants.stream()
+                .map(restaurant -> {
+                    String imageUrl = restaurantImageRepository.findFirstImageUrlByRestaurantOrderByIdAsc(restaurant);
+                    return RestaurantListResponse.from(restaurant, imageUrl);
+                })
+                .toList();
+        return responseList;
+    }
+
+    @Override
+    public List<RestaurantListResponse> getRestaurantsByDoorType(DoorType doorType) {
+        List<Restaurant> restaurants = restaurantRepository.findByDoorType(doorType);
+        if (restaurants.isEmpty()) throw new NoSuchElementException("해당 식당 리스트가 존재하지 않습니다.");
+        List<RestaurantListResponse> responseList = restaurants.stream()
+                .map(restaurant -> {
+                    String imageUrl = restaurantImageRepository.findFirstImageUrlByRestaurantOrderByIdAsc(restaurant);
+                    return RestaurantListResponse.from(restaurant, imageUrl);
+                })
+                .toList();
+        return responseList;
+    }
+
+    @Override
+    public RestaurantResponse getRestaurantById(int RestaurantId) {
+        Restaurant restaurant = restaurantRepository.findById(RestaurantId)
+                .orElseThrow(() -> new IllegalArgumentException("해당 식당이 존재하지 않습니다."));
+
+        return RestaurantResponse.from(restaurant);
+    }
 }
