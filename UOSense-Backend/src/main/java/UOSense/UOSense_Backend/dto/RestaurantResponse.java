ff59b98ca35ac2df90a1073f9731bb4c513536a3
Gomdadi <35wnsgh@naver.com>
2024-11-23 00:47:47 +0900

    feat: RestaurantResponse, RestaurantListResponse dto 클래스 구현

---
+++ UOSense-Backend/src/main/java/UOSense/UOSense_Backend/dto/RestaurantResponse.java
@@ -0,0 +1,58 @@
+package UOSense.UOSense_Backend.dto;
+
+import UOSense.UOSense_Backend.common.DoorType;
+import UOSense.UOSense_Backend.entity.Restaurant;
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+
+@Builder
+@NoArgsConstructor
+@AllArgsConstructor
+@Getter
+public class RestaurantResponse {
+    private int id;
+
+    private String name;
+
+    private DoorType doorType;
+
+    private double latitude;
+
+    private double longitude;
+
+    private String address;
+
+    private String phoneNumber;
+
+    private double rating;
+
+    private Restaurant.Category category;
+
+    private String subDescription;
+
+    private String description;
+
+    private int reviewCount;
+
+    private int bookmarkCount;
+
+    public static RestaurantResponse from(Restaurant restaurant) {
+        return RestaurantResponse.builder()
+                .id(restaurant.getId())
+                .name(restaurant.getName())
+                .doorType(restaurant.getDoorType())
+                .latitude(restaurant.getLatitude())
+                .longitude(restaurant.getLongitude())
+                .address(restaurant.getAddress())
+                .phoneNumber(restaurant.getPhoneNumber())
+                .rating(restaurant.getRating())
+                .category(restaurant.getCategory())
+                .subDescription(restaurant.getSubDescription())
+                .description(restaurant.getDescription())
+                .reviewCount(restaurant.getReviewCount())
+                .bookmarkCount(restaurant.getBookmarkCount())
+                .build();
+    }
+}
