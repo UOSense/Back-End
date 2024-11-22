9f79897fcbe49b0b060069e82db517c052c46a69
Sini0206 <a01092439672@gmail.com>
2024-11-22 12:58:26 +0900

    feat: 식당 서비스에서 메뉴 조회 메서드 정의
    - 해당 식당에 메뉴가 하나도 존재하지 않을 경우 예외 발생
    - 해당 식당 내에 존재하는 모든 메뉴를 MenuResponse DTO로 변환해서 List 타입으로 반환

--- UOSense-Backend/src/main/java/UOSense/UOSense_Backend/service/RestaurantServiceImpl.java
+++ UOSense-Backend/src/main/java/UOSense/UOSense_Backend/service/RestaurantServiceImpl.java
@@ -1,12 +1,31 @@
 package UOSense.UOSense_Backend.service;
 
+import UOSense.UOSense_Backend.dto.MenuResponse;
+import UOSense.UOSense_Backend.entity.Menu;
+import UOSense.UOSense_Backend.repository.MenuRepository;
 import UOSense.UOSense_Backend.repository.RestaurantRepository;
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Service;
+
+import java.util.List;
+import java.util.stream.Collectors;
 
 @Service
 @RequiredArgsConstructor
 public class RestaurantServiceImpl implements RestaurantService{
 
     private final RestaurantRepository restaurantRepository;
+    private final MenuRepository menuRepository;
+
+    @Override
+    public List<MenuResponse> findMenuBy(int restaurantId) {
+        List<Menu> menuBoard = menuRepository.findAllByRestaurantId((restaurantId));
+
+        if (menuBoard.isEmpty())
+            throw new IllegalArgumentException("해당 식당에 메뉴가 존재하지 않습니다.");
+
+        return menuBoard.stream()
+                .map(MenuResponse::from)
+                .collect(Collectors.toList());
+    }
 }
