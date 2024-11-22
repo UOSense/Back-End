80bcedcadd55a6e2856f6ce64c538785488d1136
Sini0206 <a01092439672@gmail.com>
2024-11-22 13:00:39 +0900

    feat: 컨트롤러에 특정 식당 메뉴 조회 메서드 정의

--- UOSense-Backend/src/main/java/UOSense/UOSense_Backend/controller/RestaurantController.java
+++ UOSense-Backend/src/main/java/UOSense/UOSense_Backend/controller/RestaurantController.java
@@ -1,11 +1,19 @@
 package UOSense.UOSense_Backend.controller;
 
+import UOSense.UOSense_Backend.dto.MenuResponse;
 import UOSense.UOSense_Backend.service.RestaurantService;
 import io.swagger.v3.oas.annotations.tags.Tag;
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
+
+import java.util.Collections;
+import java.util.List;
 
 @Tag(name = "식당 관리")
 @RestController
@@ -13,4 +21,14 @@ import org.springframework.web.bind.annotation.RestController;
 @RequiredArgsConstructor
 public class RestaurantController {
     private final RestaurantService restaurantService;
+
+    @GetMapping("/{restaurantId}/menu")
+    public ResponseEntity<List<MenuResponse>> showMenu(@PathVariable int restaurantId) {
+        try {
+            List<MenuResponse> result = restaurantService.findMenuBy(restaurantId);
+            return new ResponseEntity<>(result, HttpStatus.OK);
+        } catch (IllegalArgumentException e) {
+            return new ResponseEntity<>(Collections.emptyList(), HttpStatus.NOT_FOUND);
+        }
+    }
 }
