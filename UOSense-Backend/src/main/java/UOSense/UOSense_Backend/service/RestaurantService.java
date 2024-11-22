cd3ab3980fcfce8e9a0687bf8c7b8ff963d6bdab
Sini0206 <a01092439672@gmail.com>
2024-11-22 12:56:51 +0900

    feat(RestaurantService): 메뉴 찾는 메서드 추가
    - 식당 ID로 메뉴 찾는 기능

--- UOSense-Backend/src/main/java/UOSense/UOSense_Backend/service/RestaurantService.java
+++ UOSense-Backend/src/main/java/UOSense/UOSense_Backend/service/RestaurantService.java
@@ -1,4 +1,9 @@
 package UOSense.UOSense_Backend.service;
 
+import UOSense.UOSense_Backend.dto.MenuResponse;
+
+import java.util.List;
+
 public interface RestaurantService {
+    List<MenuResponse> findMenuBy(int restaurantId);
 }
