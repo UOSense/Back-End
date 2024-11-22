61bd144fb56b5b5349ced8a85df83b9787235b34
Sini0206 <a01092439672@gmail.com>
2024-11-22 13:04:54 +0900

    feat: MenuRequest DTO 생성

---
+++ UOSense-Backend/src/main/java/UOSense/UOSense_Backend/dto/MenuRequest.java
@@ -0,0 +1,9 @@
+package UOSense.UOSense_Backend.dto;
+
+public class MenuRequest {
+    private int restaurantId;
+    private String name;
+    private int price;
+    private String description;
+    private String image;
+}
