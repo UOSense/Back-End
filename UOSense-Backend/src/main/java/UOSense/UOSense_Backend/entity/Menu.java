863b35995149723cb42af6b2a1885d56e00c802c
Sini0206 <a01092439672@gmail.com>
2024-11-22 12:56:05 +0900

    feat: Menu 엔티티 정의

---
+++ UOSense-Backend/src/main/java/UOSense/UOSense_Backend/entity/Menu.java
@@ -0,0 +1,28 @@
+package UOSense.UOSense_Backend.entity;
+
+import jakarta.persistence.*;
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+
+@NoArgsConstructor
+@AllArgsConstructor
+@Getter
+@Entity
+public class Menu {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private int id;
+
+    @ManyToOne(fetch = FetchType.LAZY)  // Restaurant와 다대일 관계
+    @JoinColumn(name = "restaurant_id")  // 외래키 명시
+    private Restaurant restaurant;
+
+    private String name;
+
+    private int price;
+
+    private String description;
+
+    private String imageUrl;
+}
