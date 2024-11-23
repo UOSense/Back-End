b551c0fb6f6bd9d6f06ea0c989a48b6affda1089
Sini0206 <a01092439672@gmail.com>
2024-11-23 16:54:33 +0900

    style: 필요없는 import문 삭제

--- UOSense-Backend/src/main/java/UOSense/UOSense_Backend/service/RestaurantServiceImpl.java
+++ UOSense-Backend/src/main/java/UOSense/UOSense_Backend/service/RestaurantServiceImpl.java
@@ -16,7 +16,6 @@ import UOSense.UOSense_Backend.repository.RestaurantImageRepository;
 
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Service;
-import org.springframework.web.multipart.MultipartFile;
 
 import java.util.List;
 import java.util.stream.Collectors;
