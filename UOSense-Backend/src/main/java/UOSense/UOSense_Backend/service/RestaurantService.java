008961f43ee8722aa3a3b19795038bef64e77955
Sini0206 <a01092439672@gmail.com>
2024-11-23 16:54:08 +0900

    style: merge 과정에서 dto 이름 수정 안 된 부분 변경

--- UOSense-Backend/src/main/java/UOSense/UOSense_Backend/service/RestaurantService.java
+++ UOSense-Backend/src/main/java/UOSense/UOSense_Backend/service/RestaurantService.java
@@ -1,8 +1,8 @@
 package UOSense.UOSense_Backend.service;
 
 import UOSense.UOSense_Backend.common.DoorType;
+import UOSense.UOSense_Backend.dto.RestaurantInfo;
 import UOSense.UOSense_Backend.dto.RestaurantListResponse;
-import UOSense.UOSense_Backend.dto.RestaurantResponse;
 import UOSense.UOSense_Backend.dto.MenuResponse;
 import UOSense.UOSense_Backend.dto.NewMenuRequest;
 import UOSense.UOSense_Backend.entity.Restaurant;
@@ -14,7 +14,7 @@ public interface RestaurantService {
     List<RestaurantListResponse> getRestaurantsByFilter(DoorType doorType, Restaurant.Category category);
     List<RestaurantListResponse> getRestaurantsByCategory(Restaurant.Category category);
     List<RestaurantListResponse> getRestaurantsByDoorType(DoorType doorType);
-    RestaurantResponse getRestaurantById(int RestaurantId);
+    RestaurantInfo getRestaurantById(int RestaurantId);
     List<MenuResponse> findMenuBy(int restaurantId);
     void saveMenuWith(NewMenuRequest menuRequest, String imageUrl);
 }
