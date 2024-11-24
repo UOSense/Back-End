d1405f9579b536c0d248415c8266d38bc18ce2dd
Sini0206 <a01092439672@gmail.com>
2024-11-24 19:27:50 +0900

    feat: SubDescription Converter 생성
    - EnumBaseConverter 상속

---
+++ UOSense-Backend/src/main/java/UOSense/UOSense_Backend/common/SubDescriptionConverter.java
@@ -0,0 +1,7 @@
+package UOSense.UOSense_Backend.common;
+
+public class SubDescriptionConverter extends EnumBaseConverter<SubDescription>{
+    protected SubDescriptionConverter() {
+        super(SubDescription.class) ;
+    }
+}
