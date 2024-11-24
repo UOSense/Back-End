92f577dce8e1b0866a708fa955b9ea6212f54814
Sini0206 <a01092439672@gmail.com>
2024-11-24 19:28:55 +0900

    refactor(DoorType): Restaurant의 타 enum 필드들과 통일

--- UOSense-Backend/src/main/java/UOSense/UOSense_Backend/common/DoorTypeConverter.java
+++ UOSense-Backend/src/main/java/UOSense/UOSense_Backend/common/DoorTypeConverter.java
@@ -1,18 +1,10 @@
 package UOSense.UOSense_Backend.common;
 
-import UOSense.UOSense_Backend.entity.Restaurant;
-import jakarta.persistence.AttributeConverter;
 import jakarta.persistence.Converter;
 
 @Converter(autoApply = true)
-public class DoorTypeConverter implements AttributeConverter<DoorType, String> {
-    @Override
-    public String convertToDatabaseColumn(final DoorType doorType) {
-        return doorType.getName();
-    }
-
-    @Override
-    public DoorType convertToEntityAttribute(final String dbData) {
-        return DoorType.fromValue(dbData);
+public class DoorTypeConverter extends EnumBaseConverter<DoorType> {
+    protected DoorTypeConverter() {
+        super(DoorType.class);
     }
 }
