c6cb88bb9d7c9b39ad6f2c01b4507df28e3b546a
Sini0206 <a01092439672@gmail.com>
2024-11-25 05:46:10 +0900

    feat(ImageUtils): 스프링 빈으로 등록

--- UOSense-Backend/src/main/java/UOSense/UOSense_Backend/common/ImageUtilsImpl.java
+++ UOSense-Backend/src/main/java/UOSense/UOSense_Backend/common/ImageUtilsImpl.java
@@ -9,11 +9,13 @@ import com.amazonaws.services.s3.model.ObjectMetadata;
 import com.amazonaws.services.s3.model.PutObjectRequest;
 import lombok.RequiredArgsConstructor;
 import org.springframework.beans.factory.annotation.Value;
+import org.springframework.stereotype.Component;
 import org.springframework.web.multipart.MultipartFile;
 
 import java.io.IOException;
 import java.util.UUID;
 
+@Component
 @RequiredArgsConstructor
 public class ImageUtilsImpl implements ImageUtils {
     private final AmazonS3 s3Client;
