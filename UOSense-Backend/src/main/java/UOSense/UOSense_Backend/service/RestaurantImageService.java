b9e4cb8e01067660b734f4f45e4b9612f69e96bb
Sini0206 <a01092439672@gmail.com>
2024-11-25 06:21:45 +0900

    feat(RestaurantImageService): showImageList 메서드 추가

--- UOSense-Backend/src/main/java/UOSense/UOSense_Backend/service/RestaurantImageService.java
+++ UOSense-Backend/src/main/java/UOSense/UOSense_Backend/service/RestaurantImageService.java
@@ -5,9 +5,10 @@ import UOSense.UOSense_Backend.entity.Restaurant;
 import org.springframework.web.multipart.MultipartFile;
 
 public interface RestaurantImageService {
+    RestaurantImagesResponse showImageList(int restaurantId);
     /**
      * 이미지 원본 파일은 스토리지에 저장하고 DB에 저장된 URL목록을 반환합니다.
      * @throws IllegalArgumentException 저장할 엔티티가 빈 경우
      */
-    public RestaurantImagesResponse save(Restaurant restaurant, MultipartFile[] images);
+    RestaurantImagesResponse save(Restaurant restaurant, MultipartFile[] images);
 }
--- UOSense-Backend/src/main/java/UOSense/UOSense_Backend/service/RestaurantImageServiceImpl.java
+++ UOSense-Backend/src/main/java/UOSense/UOSense_Backend/service/RestaurantImageServiceImpl.java
@@ -3,6 +3,7 @@ package UOSense.UOSense_Backend.service;
 import UOSense.UOSense_Backend.common.ImageUtils;
 import UOSense.UOSense_Backend.dto.ImageInfo;
 import UOSense.UOSense_Backend.dto.RestaurantImagesResponse;
+import UOSense.UOSense_Backend.dto.RestaurantListResponse;
 import UOSense.UOSense_Backend.entity.Restaurant;
 import UOSense.UOSense_Backend.entity.RestaurantImage;
 import UOSense.UOSense_Backend.repository.RestaurantImageRepository;
@@ -12,6 +13,8 @@ import org.springframework.web.multipart.MultipartFile;
 
 import java.util.ArrayList;
 import java.util.List;
+import java.util.NoSuchElementException;
+import java.util.Optional;
 
 @Service
 @RequiredArgsConstructor
@@ -19,7 +22,18 @@ public class RestaurantImageServiceImpl implements RestaurantImageService {
     private final RestaurantImageRepository restaurantImageRepository;
     private final ImageUtils imageUtils;
     final String S3_FOLDER_NAME = "restaurant";
-    
+
+    @Override
+    public RestaurantImagesResponse showImageList(int restaurantId) {
+        List<RestaurantImage> restaurantImages = restaurantImageRepository.findAllByRestaurantId(restaurantId);
+        if (restaurantImages.isEmpty()) throw new NoSuchElementException("해당 식당에 사진이 존재하지 않습니다.");
+
+        List<ImageInfo> imageList = restaurantImages.stream()
+                .map(this::getMetaData)
+                .toList();
+        return new RestaurantImagesResponse(restaurantId, imageList);
+    }
+
     @Override
     public RestaurantImagesResponse save(Restaurant restaurant, MultipartFile[] images) {
         List<ImageInfo> imageList = new ArrayList<>();
