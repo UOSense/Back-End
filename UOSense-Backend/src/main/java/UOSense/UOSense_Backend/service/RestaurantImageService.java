1f4238f1598c0c6c2b84e7630101f4ce074581eb
Sini0206 <a01092439672@gmail.com>
2024-11-25 05:06:37 +0900

    docs(RestaurantImageService): save 메서드 주석 추가

--- UOSense-Backend/src/main/java/UOSense/UOSense_Backend/service/RestaurantImageService.java
+++ UOSense-Backend/src/main/java/UOSense/UOSense_Backend/service/RestaurantImageService.java
@@ -5,5 +5,9 @@ import UOSense.UOSense_Backend.entity.Restaurant;
 import org.springframework.web.multipart.MultipartFile;
 
 public interface RestaurantImageService {
+    /**
+     * 이미지 원본 파일은 스토리지에 저장하고 DB에 저장된 URL목록을 반환합니다.
+     * @throws IllegalArgumentException 저장할 엔티티가 빈 경우
+     */
     public RestaurantImagesResponse save(Restaurant restaurant, MultipartFile[] images);
 }
