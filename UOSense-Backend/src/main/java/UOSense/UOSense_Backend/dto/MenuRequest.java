1a733b24c7bfd70642515afe3317a014126ca4f9
Sini0206 <a01092439672@gmail.com>
2024-11-26 23:38:51 +0900

    feat: MenuRequest에 Getter 어노테이션 추가

--- UOSense-Backend/src/main/java/UOSense/UOSense_Backend/dto/MenuRequest.java
+++ UOSense-Backend/src/main/java/UOSense/UOSense_Backend/dto/MenuRequest.java
@@ -1,7 +1,8 @@
 package UOSense.UOSense_Backend.dto;
 
+import lombok.Getter;
 import org.springframework.web.multipart.MultipartFile;
-
+@Getter
 public class MenuRequest {
     private int id;
     private int restaurantId;
