8868842875db77375c9c53fba290d45f3ef91edd
Sini0206 <a01092439672@gmail.com>
2024-11-26 10:06:39 +0900

    feat(MenuService): 메뉴 수정 메서드 추가

--- UOSense-Backend/src/main/java/UOSense/UOSense_Backend/service/MenuService.java
+++ UOSense-Backend/src/main/java/UOSense/UOSense_Backend/service/MenuService.java
@@ -1,9 +1,11 @@
 package UOSense.UOSense_Backend.service;
 
+import UOSense.UOSense_Backend.dto.MenuRequest;
 import org.springframework.web.multipart.MultipartFile;
 
 public interface MenuService {
     /** 스토리지에 이미지 저장 후 URL을 반환합니다. */
     String saveImage(MultipartFile file);
+    void edit(MenuRequest menuRequest);
     void delete (int id);
 }
