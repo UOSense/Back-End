18dfd0ac08ab3dcb8017915e0d92d8b91a7cdedb
Sini0206 <a01092439672@gmail.com>
2024-11-26 23:43:36 +0900

    feat: 메뉴 수정 메서드 edit 정의

--- UOSense-Backend/src/main/java/UOSense/UOSense_Backend/service/MenuServiceImpl.java
+++ UOSense-Backend/src/main/java/UOSense/UOSense_Backend/service/MenuServiceImpl.java
@@ -1,7 +1,8 @@
 package UOSense.UOSense_Backend.service;
 
 import UOSense.UOSense_Backend.common.ImageUtils;
-import UOSense.UOSense_Backend.common.ImageUtilsImpl;
+import UOSense.UOSense_Backend.dto.MenuRequest;
+import UOSense.UOSense_Backend.entity.Restaurant;
 import UOSense.UOSense_Backend.repository.MenuRepository;
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Service;
@@ -24,12 +25,19 @@ public class MenuServiceImpl implements MenuService{
     }
 
     @Override
+    public void edit(MenuRequest menuRequest, Restaurant restaurant) {
+        if (!menuRepository.existsById(menuRequest.getId())) {
+            throw new IllegalArgumentException("수정할 메뉴가 존재하지 않습니다.");
+        }
+         menuRepository.save(menuRequest.toEntity(menuRequest,restaurant));
+    }
+
+    @Override
     public void delete(int menuId) {
         if (!menuRepository.existsById(menuId)) {
             // 댓글이 존재하지 않을 경우 예외를 던짐
             throw new IllegalArgumentException("삭제할 메뉴가 존재하지 않습니다.");
         }
-
         menuRepository.deleteById(menuId);
     }
 }
