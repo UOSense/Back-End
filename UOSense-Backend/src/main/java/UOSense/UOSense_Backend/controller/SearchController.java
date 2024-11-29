3907d8ed1d064ab4b32e604a83bca02d93b79863
Sini0206 <a01092439672@gmail.com>
2024-11-30 02:31:24 +0900

    feat: 검색 기능만 분리해서 컨트롤러 새로 생성 및 검색 기능 추가
    - 컨트롤러도 분리해보았는데 RestaurantController 내에 SearchService만 추가할지 고민

---
+++ UOSense-Backend/src/main/java/UOSense/UOSense_Backend/controller/SearchController.java
@@ -0,0 +1,46 @@
+package UOSense.UOSense_Backend.controller;
+
+import UOSense.UOSense_Backend.common.DoorType;
+import UOSense.UOSense_Backend.dto.RestaurantListResponse;
+import UOSense.UOSense_Backend.service.SearchService;
+import io.swagger.v3.oas.annotations.Operation;
+import io.swagger.v3.oas.annotations.responses.ApiResponse;
+import io.swagger.v3.oas.annotations.responses.ApiResponses;
+import io.swagger.v3.oas.annotations.tags.Tag;
+import lombok.RequiredArgsConstructor;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.RestController;
+
+import java.util.List;
+import java.util.NoSuchElementException;
+
+@Tag(name = "검색")
+@RestController
+@RequestMapping("/api/v1/Search")
+@RequiredArgsConstructor
+public class SearchController {
+    private final SearchService searchService;
+
+    @GetMapping("/restaurant")
+    @Operation(summary = "식당 검색", description = "입력어와 출입구문 필터를 바탕으로 식당을 검색합니다.")
+    @ApiResponses(value = {
+            @ApiResponse(responseCode = "200", description = "검색 결과를 성공적으로 불러왔습니다."),
+            @ApiResponse(responseCode = "400", description = "잘못된 요청입니다."),
+            @ApiResponse(responseCode = "404", description = "식당을 찾을 수 없습니다.")
+    })
+    public ResponseEntity<List<RestaurantListResponse>> searchRestarantList(@RequestParam String keyword,
+                                                            @RequestParam DoorType doorType) {
+        try {
+            List<RestaurantListResponse> restaurantListResponse = searchService.search(keyword, doorType);
+            return new ResponseEntity<>(restaurantListResponse, HttpStatus.OK);
+        } catch (NoSuchElementException e) {
+            return ResponseEntity.status(HttpStatus.NOT_FOUND).build();
+        } catch (Exception e) {
+            return ResponseEntity.badRequest().build();
+        }
+    }
+}
