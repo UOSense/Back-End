ccae22c3a772dd16ded7be59b5021bd52708c214
Sini0206 <a01092439672@gmail.com>
2024-11-30 01:52:40 +0900

    docs(SearchService): search메서드 주석 추가

--- UOSense-Backend/src/main/java/UOSense/UOSense_Backend/service/SearchService.java
+++ UOSense-Backend/src/main/java/UOSense/UOSense_Backend/service/SearchService.java
@@ -8,6 +8,9 @@ import java.util.List;
 
 public interface SearchService {
     enum sortFilter { DEFAULT, BOOKMARK, DISTANCE, RATING, REVIEW, PRICE }
+    /**
+     * 입력어와 출입구문 필터를 바탕으로 식당을 검색합니다.
+     */
     List<RestaurantListResponse> search(String keyword, DoorType doorType);
     List<Restaurant> sort(List<Restaurant> result, sortFilter filter);
 }
