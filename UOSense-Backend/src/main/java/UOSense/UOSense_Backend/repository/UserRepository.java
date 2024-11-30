9c27267c10a68f1f7220d572e2959ef3489bb863
Sini0206 <a01092439672@gmail.com>
2024-12-01 04:02:46 +0900

    feat(UserRepository): existsByEmail 선언

---
+++ UOSense-Backend/src/main/java/UOSense/UOSense_Backend/repository/UserRepository.java
@@ -0,0 +1,14 @@
+package UOSense.UOSense_Backend.repository;
+
+import UOSense.UOSense_Backend.common.BaseEnum;
+import UOSense.UOSense_Backend.entity.User;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+@Repository
+public interface UserRepository extends JpaRepository<User, Integer>{
+    /**
+     * 해당 이메일 주소로 가입된 유저가 있는지 확인합니다.
+     */
+    Boolean existsByEmail(String email);
+}
