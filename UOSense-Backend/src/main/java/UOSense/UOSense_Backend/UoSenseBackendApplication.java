b3be3be2f68a402ade9fc9585febb3470e44872a
Sini0206 <a01092439672@gmail.com>
2024-11-30 21:51:25 +0900

    feat: @EnableCaching 어노테이션을 사용해 캐시 활성화

--- UOSense-Backend/src/main/java/UOSense/UOSense_Backend/UoSenseBackendApplication.java
+++ UOSense-Backend/src/main/java/UOSense/UOSense_Backend/UoSenseBackendApplication.java
@@ -2,8 +2,10 @@ package UOSense.UOSense_Backend;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cache.annotation.EnableCaching;
 
 @SpringBootApplication
+@EnableCaching
 public class UoSenseBackendApplication {
 
     public static void main(String[] args) {
