d59e25130f455acca358066b1d0df398464a2b25
Sini0206 <a01092439672@gmail.com>
2024-11-30 23:13:13 +0900

    feat(SearchController): 검색 결과 정렬 메서드 sortByFilter 추가
    - 캐싱해서 검색 결과 있는지 확인 후 검색 결과 정렬

--- UOSense-Backend/src/main/java/UOSense/UOSense_Backend/controller/SearchController.java
+++ UOSense-Backend/src/main/java/UOSense/UOSense_Backend/controller/SearchController.java
@@ -11,10 +11,7 @@ import io.swagger.v3.oas.annotations.tags.Tag;
 import lombok.RequiredArgsConstructor;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RequestParam;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
 import java.util.List;
 import java.util.NoSuchElementException;
@@ -34,10 +31,10 @@ public class SearchController {
             @ApiResponse(responseCode = "404", description = "식당을 찾을 수 없습니다.")
     })
     public ResponseEntity<List<RestaurantListResponse>> search(@RequestParam String keyword,
-                                                                            @RequestParam DoorType closestDoor) {
+                                                               @RequestParam DoorType closestDoor) {
         try {
             List<Restaurant> restaurantList = searchService.findByKeyword(keyword); // restaurantList 캐시에 저장
-            List<Restaurant> filteredResult = searchService.filterByDoorType(keyword, closestDoor);
+            List<Restaurant> filteredResult = searchService.filterByDoorType(restaurantList, closestDoor);
             List<RestaurantListResponse> result = searchService.sort(filteredResult, SearchService.sortFilter.DEFAULT);
             return new ResponseEntity<>(result, HttpStatus.OK);
         } catch (NoSuchElementException e) {
@@ -46,6 +43,9 @@ public class SearchController {
             return ResponseEntity.badRequest().build();
         }
     }
+    /**
+     * 호출 위치: 메인 지도 화면, 식당 목록 화면
+     */
     @GetMapping("/filter")
     @Operation(summary = "검색결과 출입구문 필터링", description = "검색 결과를 출입구문 기준으로 필터링합니다.")
     @ApiResponses(value = {
@@ -54,10 +54,33 @@ public class SearchController {
             @ApiResponse(responseCode = "404", description = "식당을 찾을 수 없습니다.")
     })
     public ResponseEntity<List<RestaurantListResponse>> filterByGate(@RequestParam String keyword,
-                                                               @RequestParam DoorType doorType) {
+                                                                     @RequestParam DoorType doorType) {
         try {
-            List<Restaurant> filteredResult = searchService.filterByDoorType(keyword, doorType);
+            List<Restaurant> cachedResult = searchService.checkRestaurantCache(keyword);
+            List<Restaurant> filteredResult = searchService.filterByDoorType(cachedResult, doorType);
             List<RestaurantListResponse> result = searchService.sort(filteredResult, SearchService.sortFilter.DEFAULT);
+            return new ResponseEntity<>(result, HttpStatus.OK);
+        } catch (NoSuchElementException e) {
+            return ResponseEntity.status(HttpStatus.NOT_FOUND).build();
+        } catch (Exception e) {
+            return ResponseEntity.badRequest().build();
+        }
+    }
+    /**
+     * 호출 위치: 식당 목록 화면
+     */
+    @GetMapping("/sort")
+    @Operation(summary = "식당 목록 정렬", description = "검색 결과를 정렬합니다.")
+    @ApiResponses(value = {
+            @ApiResponse(responseCode = "200", description = "검색 결과를 성공적으로 정렬하였습니다."),
+            @ApiResponse(responseCode = "400", description = "잘못된 요청입니다."),
+            @ApiResponse(responseCode = "404", description = "식당을 찾을 수 없습니다.")
+    })
+    public ResponseEntity<List<RestaurantListResponse>> sortByFilter(@RequestParam String keyword,
+                                                                     @RequestParam SearchService.sortFilter filter) {
+        try {
+            List<Restaurant> cachedResult = searchService.checkRestaurantCache(keyword);
+            List<RestaurantListResponse> result = searchService.sort(cachedResult, filter);
             return new ResponseEntity<>(result, HttpStatus.OK);
         } catch (NoSuchElementException e) {
             return ResponseEntity.status(HttpStatus.NOT_FOUND).build();
