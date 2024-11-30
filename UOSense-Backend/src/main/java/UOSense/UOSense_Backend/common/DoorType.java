f2933a2c8969cbab4afd4c1d66c562fa14a5b148
Sini0206 <a01092439672@gmail.com>
2024-11-30 19:41:33 +0900

    feat(DoorType): 남문 추가

--- UOSense-Backend/src/main/java/UOSense/UOSense_Backend/common/DoorType.java
+++ UOSense-Backend/src/main/java/UOSense/UOSense_Backend/common/DoorType.java
@@ -1,7 +1,7 @@
 package UOSense.UOSense_Backend.common;
 
 public enum DoorType implements BaseEnum {
-    FRONT("정문"), SIDE("쪽문"), BACK("후문");
+    FRONT("정문"), SIDE("쪽문"), BACK("후문"), SOUTH("남문");
     private final String value;
 
     DoorType(String name) {
