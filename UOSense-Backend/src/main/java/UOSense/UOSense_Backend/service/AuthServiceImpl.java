36ece9327262a26a230547ba76b007cab05323af
Sini0206 <a01092439672@gmail.com>
2024-12-03 23:53:54 +0900

    refactor: 반환값 문자열로 변경, 인증코드에 알파벳도 포함

--- UOSense-Backend/src/main/java/UOSense/UOSense_Backend/service/AuthServiceImpl.java
+++ UOSense-Backend/src/main/java/UOSense/UOSense_Backend/service/AuthServiceImpl.java
@@ -1,20 +1,31 @@
 package UOSense.UOSense_Backend.service;
 
+import UOSense.UOSense_Backend.common.RedisUtil;
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Service;
+
+import java.util.Arrays;
+import java.util.NoSuchElementException;
+import java.util.Random;
 
 @Service
 @RequiredArgsConstructor
 public class AuthServiceImpl implements AuthService{
     final int CODE_LENGTH = 6;
+    Random random = new Random();
 
     @Override
-    public char[] createAuthCode() {
+    public String createAuthCode() {
         char[] code = new char[CODE_LENGTH];
         for (int i=0;i<CODE_LENGTH;i++) {
-            code[i] = (char) ('0' + (int)(Math.random() * 10));
+            int edge = random.nextInt(2);
+            if (edge == 0) {
+                code[i] = (char) random.nextInt(10);
+                continue;
+            }
+            code[i] = (char) ('A' + random.nextInt(25));
         }
-        return code;
+        return Arrays.toString(code);
     }
 
     @Override
