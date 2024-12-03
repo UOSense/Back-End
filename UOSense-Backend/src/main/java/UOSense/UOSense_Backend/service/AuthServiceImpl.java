5830c8d22008431ddcd3d76c4add2d7eeff1e56b
Sini0206 <a01092439672@gmail.com>
2024-12-04 03:21:53 +0900

    fix: 인증번호 내 숫자가 제대로 표기 안 되는 오류 해결

--- UOSense-Backend/src/main/java/UOSense/UOSense_Backend/service/AuthServiceImpl.java
+++ UOSense-Backend/src/main/java/UOSense/UOSense_Backend/service/AuthServiceImpl.java
@@ -21,12 +21,12 @@ public class AuthServiceImpl implements AuthService{
         for (int i=0;i<CODE_LENGTH;i++) {
             int edge = random.nextInt(2);
             if (edge == 0) {
-                code[i] = (char) random.nextInt(10);
+                code[i] = (char) ('0' + random.nextInt(10));
                 continue;
             }
-            code[i] = (char) ('A' + random.nextInt(25));
+            code[i] = (char) ('A' + random.nextInt(26));
         }
-        return Arrays.toString(code);
+        return new String(code);
     }
 
     @Override
