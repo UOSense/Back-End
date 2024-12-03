dc2276de8d6c62eb334cb212d6a8f2af40b9c20c
Sini0206 <a01092439672@gmail.com>
2024-12-04 03:27:45 +0900

    feat: sendAutoMail 메서드에 메일 형식 체크 로직 추가

--- UOSense-Backend/src/main/java/UOSense/UOSense_Backend/service/MailServiceImpl.java
+++ UOSense-Backend/src/main/java/UOSense/UOSense_Backend/service/MailServiceImpl.java
@@ -36,6 +36,7 @@ public class MailServiceImpl implements MailService{
     @Override
     public void sendAuthMail(String email, String purpose, String authCode) {
         try {
+            if (!checkMailAddress(email)) return;
             String body = "<html><body style=\"text-align: center;\"><h2> 인증번호 [ "+ authCode +" ]</h2>" +
                     "<b>시대생 맛집 지도 "+ purpose +" 화면에서 인증번호를 입력해 주세요.</b></body></html>";
             MimeMessage newMail = emailUtil.createMail(email,purpose +" 인증 번호", body);
