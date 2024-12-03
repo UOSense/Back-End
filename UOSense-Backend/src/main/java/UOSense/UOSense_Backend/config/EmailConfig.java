7763e6c8b71f873009e3f6cf04ced064f6c6a5d1
Sini0206 <a01092439672@gmail.com>
2024-12-03 21:47:19 +0900

    feat: 메일 속성 get메서드에 인증코드 만료시간 추가

--- UOSense-Backend/src/main/java/UOSense/UOSense_Backend/config/EmailConfig.java
+++ UOSense-Backend/src/main/java/UOSense/UOSense_Backend/config/EmailConfig.java
@@ -61,6 +61,7 @@ public class EmailConfig {
         properties.put("mail.smtp.auth", auth);
         properties.put("mail.smtp.starttls.enable", starttlsEnable);
         properties.put("mail.smtp.starttls.required", starttlsRequired);
+        properties.put("auth-code-expiration-millis", authCodeExpirationMillis);
         //properties.put("mail.smtp.connectiontimeout", connectionTimeout);
         //properties.put("mail.smtp.timeout", timeout);
         //properties.put("mail.smtp.writetimeout", writeTimeout);
