c1a3141b807ae0d0893161041827cff6b789222c
Gomdadi <35wnsgh@naver.com>
2024-12-05 04:00:06 +0900

    feat: User 서비스, 인증을 위해 spring security에서 정의한 인터페이스 UserDetailsService를 상속

---
+++ UOSense-Backend/src/main/java/UOSense/UOSense_Backend/service/UserServiceImpl.java
@@ -0,0 +1,28 @@
+package UOSense.UOSense_Backend.service;
+import UOSense.UOSense_Backend.common.securityFilter.CustomUserDetails;
+import UOSense.UOSense_Backend.entity.User;
+import UOSense.UOSense_Backend.repository.UserRepository;
+import lombok.RequiredArgsConstructor;
+import org.springframework.security.core.userdetails.UserDetails;
+import org.springframework.security.core.userdetails.UserDetailsService;
+import org.springframework.security.core.userdetails.UsernameNotFoundException;
+import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
+
+@Service
+@Transactional(readOnly = true)
+@RequiredArgsConstructor
+public class UserServiceImpl implements UserService, UserDetailsService {
+
+    private final UserRepository userRepository;
+
+    @Override
+    public UserDetails loadUserByUsername(String email) throws UsernameNotFoundException {
+        User user = userRepository.findByEmail(email);
+        if (user == null) {
+            throw new UsernameNotFoundException("Invalid authentication!");
+        }
+
+        return new CustomUserDetails(user);
+    }
+}
