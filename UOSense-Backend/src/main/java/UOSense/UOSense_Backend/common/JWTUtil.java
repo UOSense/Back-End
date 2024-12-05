a8aecfb7924ef6271c51afcff4a42fb1ab65689c
Gomdadi <35wnsgh@naver.com>
2024-12-05 20:52:44 +0900

    refactor: UserController에 있던 로직 코드 JwtUtil 클래스의 메서드로 이동

--- UOSense-Backend/src/main/java/UOSense/UOSense_Backend/common/JWTUtil.java
+++ UOSense-Backend/src/main/java/UOSense/UOSense_Backend/common/JWTUtil.java
@@ -1,23 +1,37 @@
 package UOSense.UOSense_Backend.common;
 
+import io.jsonwebtoken.ExpiredJwtException;
 import io.jsonwebtoken.Jwts;
 import jakarta.servlet.http.Cookie;
+import jakarta.servlet.http.HttpServletRequest;
+import org.apache.coyote.BadRequestException;
 import org.springframework.beans.factory.annotation.Value;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
 import org.springframework.stereotype.Component;
 
 import javax.crypto.SecretKey;
 import javax.crypto.spec.SecretKeySpec;
 import java.nio.charset.StandardCharsets;
+import java.time.Duration;
 import java.util.Date;
+import java.util.HashMap;
+import java.util.Map;
 
 @Component
 public class JWTUtil {
 
     private final SecretKey secretKey;
+    private final RedisUtilForToken redisUtilForToken;
+    @Value("${jwt.access.duration}")
+    private long accessDuration;
+    @Value("${jwt.refresh.duration}")
+    private long refreshDuration;
 
     // 암호화 알고리즘은 HS256을 사용
-    public JWTUtil(@Value("${jwt.secret}") String secret) {
+    public JWTUtil(@Value("${jwt.secret}") String secret, RedisUtilForToken redisUtilForToken) {
         this.secretKey = new SecretKeySpec(secret.getBytes(StandardCharsets.UTF_8), Jwts.SIG.HS256.key().build().getAlgorithm());
+        this.redisUtilForToken = redisUtilForToken;
     }
 
     /**
@@ -70,5 +84,73 @@ public class JWTUtil {
         //cookie.setHttpOnly(true);       // http에서만 쿠키가 동작할 수 있도록 (js에서 접근할 수 없음)
 
         return cookie;
+    }
+
+    public String parseCookie(HttpServletRequest request) throws BadRequestException {
+        // 쿠키에 존재하는 refreshToken을 가져오자
+        String refresh = null;
+        Cookie[] cookies = request.getCookies();
+        if(cookies == null) {
+            throw new BadRequestException("Cookies is null");
+        }
+        for(Cookie cookie : cookies) {
+            if("refresh".equals(cookie.getName())) {
+                refresh = cookie.getValue();
+            }
+        }
+
+        // 검증 시작
+        // refreshToken이 없는 경우
+        if(refresh == null) {
+            throw new BadRequestException("Refresh token is null");
+        }
+
+        // 유효기간 확인
+        // 토큰이 refresh인지 확인
+        String category;
+        try {
+            category = getCategory(refresh);
+        } catch (ExpiredJwtException e) {
+            throw new BadRequestException("Refresh token is expired");
+        }
+
+        if(!category.equals("refresh")) {
+            throw new BadRequestException("Invalid refresh token");
+        }
+
+        return refresh;
+    }
+
+    public Map<String, String> createTokens(String refresh) {
+        // 새로운 Token을 만들기 위해 준비
+        String email = getEmail(refresh);
+        Role role = getRole(refresh);
+
+        // Redis내에 존재하는 refreshToken인지 확인
+        if(!redisUtilForToken.checkExistsToken(email)) {
+            throw new IllegalArgumentException("No exists in redis refresh token");
+        }
+
+        // 새로운 JWT Token 생성
+        String newAccessToken = createJwt("access", email, role, accessDuration);
+        String newRefreshToken = createJwt("refresh", email, role, refreshDuration);
+
+        // update refreshToken to Redis
+        redisUtilForToken.setToken(email, newRefreshToken, Duration.ofMillis(refreshDuration));
+
+        Map<String, String> tokens = new HashMap<>();
+        tokens.put("access", newAccessToken);
+        tokens.put("refresh", newRefreshToken);
+        return tokens;
+    }
+
+    public void deleteRefresh(String refresh) {
+        String key = getEmail(refresh);
+
+        if(!redisUtilForToken.checkExistsToken(key)) {
+            throw new IllegalArgumentException("No exists in redis refresh token");
+        }
+
+        redisUtilForToken.deleteToken(key);
     }
 }
--- UOSense-Backend/src/main/java/UOSense/UOSense_Backend/controller/UserController.java
+++ UOSense-Backend/src/main/java/UOSense/UOSense_Backend/controller/UserController.java
@@ -1,20 +1,15 @@
 package UOSense.UOSense_Backend.controller;
 
 import UOSense.UOSense_Backend.common.JWTUtil;
-import UOSense.UOSense_Backend.common.RedisUtilForToken;
-import UOSense.UOSense_Backend.common.Role;
-import io.jsonwebtoken.ExpiredJwtException;
 import io.swagger.v3.oas.annotations.tags.Tag;
-import jakarta.servlet.http.Cookie;
 import jakarta.servlet.http.HttpServletRequest;
 import jakarta.servlet.http.HttpServletResponse;
 import lombok.RequiredArgsConstructor;
-import org.springframework.beans.factory.annotation.Value;
+import org.apache.coyote.BadRequestException;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 
-import java.time.Duration;
 import java.util.HashMap;
 import java.util.Map;
 
@@ -24,75 +19,26 @@ import java.util.Map;
 @RequiredArgsConstructor
 public class UserController {
     private final JWTUtil jwtUtil;
-    private final RedisUtilForToken redisUtilForToken;
-    @Value("${jwt.access.duration}")
-    private long accessDuration;
-    @Value("${jwt.refresh.duration}")
-    private long refreshDuration;
 
     @GetMapping("/reissue")
     public ResponseEntity<?> reissue(HttpServletRequest request, HttpServletResponse response) {
-
-        // 쿠키에 존재하는 refreshToken을 가져오자
-        String refresh = null;
-        Cookie[] cookies = request.getCookies();
-        if(cookies == null) {
-            Map<String, Object> message = new HashMap<>();
-            message.put("message", "Cookie is null");
-            return new ResponseEntity<>(message, HttpStatus.BAD_REQUEST);
-        }
-        for(Cookie cookie : cookies) {
-            if("refresh".equals(cookie.getName())) {
-                refresh = cookie.getValue();
-            }
-        }
-
-        // 검증 시작
-        // refreshToken이 없는 경우
-        if(refresh == null) {
-            Map<String, Object> message = new HashMap<>();
-            message.put("message", "Refresh token is null");
-            return new ResponseEntity<>(message, HttpStatus.BAD_REQUEST);
-        }
-
-        // 유효기간 확인
-        // 토큰이 refresh인지 확인
-        String category;
+        String refresh;
         try {
-            category = jwtUtil.getCategory(refresh);
-        } catch (ExpiredJwtException e) {
-            Map<String, Object> message = new HashMap<>();
-            message.put("message", "Refresh token is expired");
-            return new ResponseEntity<>(message, HttpStatus.BAD_REQUEST);
+            refresh = jwtUtil.parseCookie(request);
+        } catch (BadRequestException e) {
+            return new ResponseEntity<>(e.getMessage(), HttpStatus.BAD_REQUEST);
         }
 
-        if(!category.equals("refresh")) {
-            Map<String, Object> message = new HashMap<>();
-            message.put("message", "Invalid refresh token");
-            return new ResponseEntity<>(message, HttpStatus.BAD_REQUEST);
+        Map<String, String> tokens;
+        try {
+            tokens = jwtUtil.createTokens(refresh);
+        } catch (IllegalStateException e) {
+            return new ResponseEntity<>(e.getMessage(), HttpStatus.BAD_REQUEST);
         }
-
-        // 새로운 Token을 만들기 위해 준비
-        String email = jwtUtil.getEmail(refresh);
-        Role role = jwtUtil.getRole(refresh);
-
-        // Redis내에 존재하는 refreshToken인지 확인
-        if(!redisUtilForToken.checkExistsToken(email)) {
-            Map<String, Object> message = new HashMap<>();
-            message.put("message", "No exists in redis refresh token");
-            return new ResponseEntity<>(message, HttpStatus.BAD_REQUEST);
-        }
-
-        // 새로운 JWT Token 생성
-        String newAccessToken = jwtUtil.createJwt("access", email, role, accessDuration);
-        String newRefreshToken = jwtUtil.createJwt("refresh", email, role, refreshDuration);
-
-        // update refreshToken to Redis
-        redisUtilForToken.setToken(email, newRefreshToken, Duration.ofMillis(refreshDuration));
 
         // 응답
-        response.setHeader("access", "Bearer " + newAccessToken);
-        response.addCookie(jwtUtil.createCookie("refresh", newRefreshToken));
+        response.setHeader("access", "Bearer " + tokens.get("access"));
+        response.addCookie(jwtUtil.createCookie("refresh", tokens.get("refresh")));
 
         Map<String, Object> message = new HashMap<>();
         message.put("message", "Reissue refresh token success");
@@ -104,50 +50,18 @@ public class UserController {
      */
     @PutMapping("/signout")
     public ResponseEntity<?> signOut(HttpServletRequest request) {
-        String refresh = null;
-        Cookie[] cookies = request.getCookies();
-        if(cookies == null) {
-            Map<String, Object> message = new HashMap<>();
-            message.put("message", "Cookie is null");
-            return new ResponseEntity<>(message, HttpStatus.BAD_REQUEST);
-        }
-
-        for(Cookie cookie : cookies) {
-            if("refresh".equals(cookie.getName())) {
-                refresh = cookie.getValue();
-            }
-        }
-
-        if(refresh == null) {
-            Map<String, Object> message = new HashMap<>();
-            message.put("message", "Refresh token is null");
-            return new ResponseEntity<>(message, HttpStatus.BAD_REQUEST);
-        }
-
-        String category;
+        String refresh;
         try {
-            category = jwtUtil.getCategory(refresh);
-        } catch (ExpiredJwtException e) {
-            Map<String, Object> message = new HashMap<>();
-            message.put("message", "Refresh token expired");
-            return new ResponseEntity<>(message, HttpStatus.BAD_REQUEST);
+            refresh = jwtUtil.parseCookie(request);
+        } catch (BadRequestException e) {
+            return new ResponseEntity<>(e.getMessage(), HttpStatus.BAD_REQUEST);
         }
 
-        if(!category.equals("refresh")) {
-            Map<String, Object> message = new HashMap<>();
-            message.put("message", "Invalid refresh token");
-            return new ResponseEntity<>(message, HttpStatus.BAD_REQUEST);
+        try {
+            jwtUtil.deleteRefresh(refresh);
+        } catch (IllegalStateException e) {
+            return new ResponseEntity<>(e.getMessage(), HttpStatus.BAD_REQUEST);
         }
-
-        String key = jwtUtil.getEmail(refresh);
-
-        if(!redisUtilForToken.checkExistsToken(key)) {
-            Map<String, Object> message = new HashMap<>();
-            message.put("message", "No exists in redis refresh token");
-            return new ResponseEntity<>(message, HttpStatus.BAD_REQUEST);
-        }
-
-        redisUtilForToken.deleteToken(key);
 
         Map<String, Object> message = new HashMap<>();
         message.put("message", "Signed out successfully");
