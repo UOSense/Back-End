d904cc1ce8ac1725101e9f60809844ceb31bde9c
Sini0206 <a01092439672@gmail.com>
2024-12-07 04:46:39 +0900

    feat: Report 리파지토리 생성

---
+++ UOSense-Backend/src/main/java/UOSense/UOSense_Backend/repository/ReportRepository.java
@@ -0,0 +1,7 @@
+package UOSense.UOSense_Backend.repository;
+
+import UOSense.UOSense_Backend.entity.Report;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface ReportRepository extends JpaRepository<Report, Integer> {
+}
