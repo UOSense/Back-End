3eafd964c2ca5c99d234e6e84e17b8a6e2fe2127
Sini0206 <a01092439672@gmail.com>
2024-12-06 16:33:51 +0900

    feat:  ReviewImage 리파지토리 생성

---
+++ UOSense-Backend/src/main/java/UOSense/UOSense_Backend/repository/ReviewImageRepository.java
@@ -0,0 +1,7 @@
+package UOSense.UOSense_Backend.repository;
+
+import UOSense.UOSense_Backend.entity.ReviewImage;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface ReviewImageRepository extends JpaRepository<ReviewImage, Integer> {
+}
