43cfc3b18afa3b108b329a301e19614e5966a88c
Sini0206 <a01092439672@gmail.com>
2024-12-06 20:55:11 +0900

    feat(ReviewRepository): 식당 ID로 DB에서 리뷰 찾는 메서드 추가

--- UOSense-Backend/src/main/java/UOSense/UOSense_Backend/repository/ReviewRepository.java
+++ UOSense-Backend/src/main/java/UOSense/UOSense_Backend/repository/ReviewRepository.java
@@ -3,5 +3,8 @@ package UOSense.UOSense_Backend.repository;
 import UOSense.UOSense_Backend.entity.Review;
 import org.springframework.data.jpa.repository.JpaRepository;
 
+import java.util.List;
+
 public interface ReviewRepository extends JpaRepository<Review, Integer> {
+    List<Review> findAllByRestaurantId(int restaurantId);
 }
