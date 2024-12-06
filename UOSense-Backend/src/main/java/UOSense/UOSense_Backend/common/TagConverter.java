21c75c7cf2291836297ef19436fc4eadd87cdd5c
Sini0206 <a01092439672@gmail.com>
2024-12-07 04:44:08 +0900

    feat: TagConverter에 Converter 어노테이션 추가

--- UOSense-Backend/src/main/java/UOSense/UOSense_Backend/common/TagConverter.java
+++ UOSense-Backend/src/main/java/UOSense/UOSense_Backend/common/TagConverter.java
@@ -1,5 +1,8 @@
 package UOSense.UOSense_Backend.common;
 
+import jakarta.persistence.Converter;
+
+@Converter(autoApply = true)
 public class TagConverter extends EnumBaseConverter<Tag> {
     public TagConverter() {
         super(Tag.class) ;
