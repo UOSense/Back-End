d0fb8d4d7ac4e7636b9c19b0491063086cad09d7
Sini0206 <a01092439672@gmail.com>
2024-12-06 16:34:59 +0900

    feat:  BookMark 컨트롤러 생성

---
+++ UOSense-Backend/src/main/java/UOSense/UOSense_Backend/controller/BookMarkController.java
@@ -0,0 +1,15 @@
+package UOSense.UOSense_Backend.controller;
+
+import UOSense.UOSense_Backend.service.BookMarkService;
+import io.swagger.v3.oas.annotations.tags.Tag;
+import lombok.RequiredArgsConstructor;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@Tag(name = "즐겨찾기 관리")
+@RestController
+@RequestMapping("/api/v1/BookMark")
+@RequiredArgsConstructor
+public class BookMarkController {
+    private final BookMarkService bookMarkService;
+}
