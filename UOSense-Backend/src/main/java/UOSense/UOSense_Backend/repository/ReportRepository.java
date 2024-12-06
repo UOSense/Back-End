4b1ebe89f29d127b55d66cfb08cbb695c6e133b4
Sini0206 <a01092439672@gmail.com>
2024-12-07 04:54:12 +0900

    feat: Repository 어노테이션 추가

--- UOSense-Backend/src/main/java/UOSense/UOSense_Backend/repository/ReportRepository.java
+++ UOSense-Backend/src/main/java/UOSense/UOSense_Backend/repository/ReportRepository.java
@@ -2,6 +2,8 @@ package UOSense.UOSense_Backend.repository;
 
 import UOSense.UOSense_Backend.entity.Report;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
 
+@Repository
 public interface ReportRepository extends JpaRepository<Report, Integer> {
 }
