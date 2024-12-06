d07f7d2720e9711751ed73dc565dbe5712db6f30
Sini0206 <a01092439672@gmail.com>
2024-12-06 16:33:42 +0900

    feat:  Review 리파지토리 생성

---
+++ UOSense-Backend/src/main/java/UOSense/UOSense_Backend/repository/ReviewRepository.java
@@ -0,0 +1,7 @@
+package UOSense.UOSense_Backend.repository;
+
+import UOSense.UOSense_Backend.entity.Review;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface ReviewRepository extends JpaRepository<Review, Integer> {
+}
