7f5ed9a3da103379d21619c0070c96dab9e22ddd
Sini0206 <a01092439672@gmail.com>
2024-12-07 04:46:05 +0900

    feat(ReviewRepository): Repository 어노테이션 추가

--- UOSense-Backend/src/main/java/UOSense/UOSense_Backend/repository/ReviewRepository.java
+++ UOSense-Backend/src/main/java/UOSense/UOSense_Backend/repository/ReviewRepository.java
@@ -2,6 +2,8 @@ package UOSense.UOSense_Backend.repository;
 
 import UOSense.UOSense_Backend.entity.Review;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
 
+@Repository
 public interface ReviewRepository extends JpaRepository<Review, Integer> {
 }
