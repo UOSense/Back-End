c8858d7807dfdd819828eec5f68be8d92f278390
Sini0206 <a01092439672@gmail.com>
2024-12-06 19:22:57 +0900

    feat: 리뷰 삭제 API 추가

--- UOSense-Backend/src/main/java/UOSense/UOSense_Backend/controller/ReviewController.java
+++ UOSense-Backend/src/main/java/UOSense/UOSense_Backend/controller/ReviewController.java
@@ -4,6 +4,9 @@ import UOSense.UOSense_Backend.service.ReviewImageService;
 import UOSense.UOSense_Backend.service.ReviewService;
 import io.swagger.v3.oas.annotations.tags.Tag;
 import lombok.RequiredArgsConstructor;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.DeleteMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
@@ -14,4 +17,16 @@ import org.springframework.web.bind.annotation.RestController;
 public class ReviewController {
     private final ReviewService reviewService;
     private final ReviewImageService reviewImageService;
+
+    @DeleteMapping("/delete/{reviewId}")
+    public ResponseEntity<Void> delete(@PathVariable int reviewId) {
+        try {
+            reviewService.delete(reviewId);
+            return ResponseEntity.ok().build();
+        } catch (IllegalArgumentException e) {
+            return ResponseEntity.notFound().build();
+        } catch (Exception e) {
+            return ResponseEntity.badRequest().build();
+        }
+    }
 }
