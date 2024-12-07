651de39f7988f0851e6587bb1e891aefc42786a8
Gomdadi <35wnsgh@naver.com>
2024-12-08 01:51:37 +0900

    fix: JSON 직렬화 에러 픽스

--- UOSense-Backend/src/main/java/UOSense/UOSense_Backend/dto/ReportResponse.java
+++ UOSense-Backend/src/main/java/UOSense/UOSense_Backend/dto/ReportResponse.java
@@ -1,28 +1,31 @@
 package UOSense.UOSense_Backend.dto;
 
 import UOSense.UOSense_Backend.entity.Report;
-import UOSense.UOSense_Backend.entity.Review;
-import UOSense.UOSense_Backend.entity.User;
 import lombok.AllArgsConstructor;
 import lombok.Builder;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
 
 import java.time.LocalDateTime;
+
 @Builder
 @AllArgsConstructor
+@NoArgsConstructor
+@Getter
 public class ReportResponse {
     private int id;
-    private Review review;
+    private int reviewId;
     private String title;
-    private User user;
+    private int userId;
     private String detail;
     private LocalDateTime createdAt;
 
     public static ReportResponse from(Report report) {
         return builder()
                 .id(report.getId())
-                .review(report.getReview())
+                .reviewId(report.getReview().getId())
                 .title(report.getTitle())
-                .user(report.getUser())
+                .userId(report.getUser().getId())
                 .detail(report.getDetail().getValue())
                 .createdAt(report.getCreatedAt())
                 .build();
