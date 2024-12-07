8ad583d2270934ae44c0ce085614c3a80c705144
Sini0206 <a01092439672@gmail.com>
2024-12-07 16:28:53 +0900

    feat(reportService): 신고 일괄 조회 메서드 findList 정의
    - 관리자만 접근 가능하도록 함

--- UOSense-Backend/src/main/java/UOSense/UOSense_Backend/service/ReportServiceImpl.java
+++ UOSense-Backend/src/main/java/UOSense/UOSense_Backend/service/ReportServiceImpl.java
@@ -1,11 +1,34 @@
 package UOSense.UOSense_Backend.service;
 
+import UOSense.UOSense_Backend.common.Role;
+import UOSense.UOSense_Backend.dto.CustomUserDetails;
+import UOSense.UOSense_Backend.dto.ReportResponse;
 import UOSense.UOSense_Backend.repository.ReportRepository;
 import lombok.RequiredArgsConstructor;
+import org.springframework.security.core.Authentication;
 import org.springframework.stereotype.Service;
+import org.springframework.security.access.AccessDeniedException;
+
+import java.util.List;
 
 @Service
 @RequiredArgsConstructor
 public class ReportServiceImpl implements ReportService{
     private final ReportRepository reportRepository;
+
+    @Override
+    public List<ReportResponse> findList(Authentication authentication) {
+        CustomUserDetails customUserDetails = (CustomUserDetails) authentication.getPrincipal();
+
+        boolean isAdmin = customUserDetails.getAuthorities().stream()
+                .anyMatch(authority -> authority.getAuthority().equals(Role.ADMIN.getValue()));
+
+        if (!isAdmin) {
+            throw new AccessDeniedException("관리자만 접근할 수 있습니다.");
+        }
+
+        return reportRepository.findAll().stream()
+                .map(ReportResponse::from)
+                .toList();
+    }
 }
