1d422a07d91ff21f0b8f21389800757ed85ffdf2
Sini0206 <a01092439672@gmail.com>
2024-12-07 23:37:37 +0900

    feat:(ReviewRepository): increaseLikeCount 메서드 선언
    - set함수 쓰는 것보다 데이터베이스 레벨에서 처리하는 게 더 안전하다고 하여 선언
    - 영향 받은 row 수 반환

--- UOSense-Backend/src/main/java/UOSense/UOSense_Backend/repository/ReviewRepository.java
+++ UOSense-Backend/src/main/java/UOSense/UOSense_Backend/repository/ReviewRepository.java
@@ -2,9 +2,15 @@ package UOSense.UOSense_Backend.repository;
 
 import UOSense.UOSense_Backend.entity.Review;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Modifying;
+import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.repository.query.Param;
 
 import java.util.List;
 
 public interface ReviewRepository extends JpaRepository<Review, Integer> {
     List<Review> findAllByRestaurantId(int restaurantId);
+    @Modifying
+    @Query("UPDATE Review r SET r.likeCount = r.likeCount + 1 WHERE r.id = :reviewId")
+    int increaseLikeCount(@Param("reviewId") int reviewId);
 }
