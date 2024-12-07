8b0a711d8f4e294da6482c6a7d047447048c5e85
Sini0206 <a01092439672@gmail.com>
2024-12-08 01:06:40 +0900

    feat: Repository 어노테이션 빼먹어서 추가

--- UOSense-Backend/src/main/java/UOSense/UOSense_Backend/repository/ReviewLikeRepository.java
+++ UOSense-Backend/src/main/java/UOSense/UOSense_Backend/repository/ReviewLikeRepository.java
@@ -2,7 +2,9 @@ package UOSense.UOSense_Backend.repository;
 
 import UOSense.UOSense_Backend.entity.ReviewLike;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
 
+@Repository
 public interface ReviewLikeRepository extends JpaRepository<ReviewLike, Long> {
     boolean existsByUserIdAndReviewId(int userId, int reviewId);
 }
