f6e0674989ef421868f0462ec15ed8ab398160dd
Gomdadi <35wnsgh@naver.com>
2024-12-08 17:41:58 +0900

    feat: 정보 수정 제안 엔티티 생성

---
+++ UOSense-Backend/src/main/java/UOSense/UOSense_Backend/entity/PurposeBusinessDay.java
@@ -0,0 +1,52 @@
+package UOSense.UOSense_Backend.entity;
+
+import jakarta.persistence.*;
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+
+import java.time.LocalTime;
+
+@Builder
+@NoArgsConstructor
+@AllArgsConstructor
+@Getter
+@Entity
+@Table(name = "Purpose_BusinessDay")
+public class PurposeBusinessDay {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private int id;
+
+    @ManyToOne(fetch = FetchType.LAZY)  // Restaurant와 다대일 관계
+    @JoinColumn(name = "restaurant_id")  // 외래키 명시
+    private PurposeRestaurant purposeRestaurant;
+
+    @Column(name = "day_of_week", nullable = false)
+    @Enumerated(EnumType.STRING)
+    private DayOfWeek dayOfWeek;
+    public enum DayOfWeek {Monday, Tuesday, Wednesday, Thursday, Friday, Saturday, Sunday}
+
+    @Column(name = "have_break_time", nullable = false)
+    private boolean haveBreakTime;
+
+    @Column(name = "start_break_time")
+    private LocalTime startBreakTime;
+
+    @Column(name = "stop_break_time")
+    private LocalTime stopBreakTime;
+
+    @Column(name = "opening_time")
+    private LocalTime openingTime;
+
+    @Column(name ="closing_time")
+    private LocalTime closingTime;
+
+    @Column(name = "is_holiday", nullable = false)
+    private boolean isHoliday;
+
+    @ManyToOne(fetch = FetchType.LAZY)  // User와 다대일 관계
+    @JoinColumn(name = "user_id")  // 외래키 명시
+    private User user;
+}
---
+++ UOSense-Backend/src/main/java/UOSense/UOSense_Backend/entity/PurposeRestaurant.java
@@ -0,0 +1,36 @@
+package UOSense.UOSense_Backend.entity;
+
+import UOSense.UOSense_Backend.common.converter.SubDescriptionConverter;
+import UOSense.UOSense_Backend.common.enumClass.SubDescription;
+import jakarta.persistence.*;
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+
+@Builder
+@NoArgsConstructor
+@AllArgsConstructor
+@Getter
+@Entity
+@Table(name = "Purpose_Restaurant")
+public class PurposeRestaurant {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private int id;
+
+    private String name;
+
+    private String address;
+
+    @Column(name = "phone_number")
+    private String phoneNumber;
+
+    @Convert(converter = SubDescriptionConverter.class)
+    @Column(name = "sub_description")
+    private SubDescription subDescription;
+
+    @ManyToOne(fetch = FetchType.LAZY)  // User와 다대일 관계
+    @JoinColumn(name = "user_id")  // 외래키 명시
+    private User user;
+}
