e7f640ce367c22d99a0a892a576d49e0acfd0695
Gomdadi <35wnsgh@naver.com>
2024-12-13 02:50:31 +0900

    feat: 영업 정보 조회 Repository 메서드

--- UOSense-Backend/src/main/java/UOSense/UOSense_Backend/repository/PurposeDayRepository.java
+++ UOSense-Backend/src/main/java/UOSense/UOSense_Backend/repository/PurposeDayRepository.java
@@ -2,11 +2,16 @@ package UOSense.UOSense_Backend.repository;
 
 import UOSense.UOSense_Backend.entity.PurposeDay;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.repository.query.Param;
 import org.springframework.stereotype.Repository;
 
 import java.util.List;
 
 @Repository
 public interface PurposeDayRepository extends JpaRepository<PurposeDay, Integer> {
-//    List<PurposeDay> findAllByRestaurantId(int restaurantId);
+    @Query(value = "SELECT b.* " +
+            "FROM Purpose_BusinessDay b " +
+            "WHERE b.restaurant_id = :restaurantId " , nativeQuery = true)
+    List<PurposeDay> findAllByRestaurantId(@Param("restaurantId") int restaurantId);
 }
