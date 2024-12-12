a3c9d72a254175a53affb69b66c1f870a0d675ca
Gomdadi <35wnsgh@naver.com>
2024-12-13 02:55:36 +0900

    feat: PurposeDayInfo, PurposeDayList DTO - BusinessDayInfo, BusinessDayList DTO와 구조가 같음

---
+++ UOSense-Backend/src/main/java/UOSense/UOSense_Backend/dto/PurposeDayInfo.java
@@ -0,0 +1,83 @@
+package UOSense.UOSense_Backend.dto;
+
+import UOSense.UOSense_Backend.entity.BusinessDay;
+import UOSense.UOSense_Backend.entity.PurposeDay;
+import UOSense.UOSense_Backend.entity.PurposeRest;
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+
+import java.time.LocalTime;
+
+@Builder
+@NoArgsConstructor
+@AllArgsConstructor
+@Getter
+public class PurposeDayInfo {
+    private int id;
+
+    private BusinessDay.DayOfWeek dayOfWeek;
+
+    private boolean breakTime;
+
+    private String startBreakTime;
+
+    private String stopBreakTime;
+
+    private String openingTime;
+
+    private String closingTime;
+
+    private boolean holiday;
+
+    public void setBreakTime() {
+        this.startBreakTime = null;
+        this.stopBreakTime = null;
+    }
+
+    public void setTime() {
+        this.startBreakTime = null;
+        this.stopBreakTime = null;
+        this.openingTime = null;
+        this.closingTime = null;
+    }
+
+    public static PurposeDayInfo from(PurposeDay purposeDay) {
+        return PurposeDayInfo.builder()
+                .id(purposeDay.getId())
+                .dayOfWeek(purposeDay.getDayOfWeek())
+                .breakTime(purposeDay.isBreakTime())
+                .startBreakTime(String.valueOf(purposeDay.getStartBreakTime()))
+                .stopBreakTime(String.valueOf(purposeDay.getStopBreakTime()))
+                .openingTime(String.valueOf(purposeDay.getOpeningTime()))
+                .closingTime(String.valueOf(purposeDay.getClosingTime()))
+                .holiday(purposeDay.isHoliday())
+                .build();
+    }
+
+    public static PurposeDay toEntity(PurposeDayInfo purposeDayInfo, PurposeRest purposeRest) {
+        LocalTime startBreakTime = (purposeDayInfo.getStartBreakTime() != null) ? LocalTime.parse(purposeDayInfo.getStartBreakTime()) : null;
+        LocalTime stopBreakTime = (purposeDayInfo.getStopBreakTime() != null) ? LocalTime.parse(purposeDayInfo.getStopBreakTime()) : null;
+        LocalTime openingTime = (purposeDayInfo.getOpeningTime() != null) ? LocalTime.parse(purposeDayInfo.getOpeningTime()) : null;
+        LocalTime closingTime = (purposeDayInfo.getClosingTime() != null) ? LocalTime.parse(purposeDayInfo.getClosingTime()) : null;
+        PurposeDay purposeDay = PurposeDay.builder()
+                .purposeRest(purposeRest)
+                .dayOfWeek(purposeDayInfo.getDayOfWeek())
+                .breakTime(purposeDayInfo.isBreakTime())
+                .startBreakTime(startBreakTime)
+                .stopBreakTime(stopBreakTime)
+                .openingTime(openingTime)
+                .closingTime(closingTime)
+                .holiday(purposeDayInfo.isHoliday())
+                .build();
+        if (purposeDayInfo.getId() == -1) {
+            // 신규 엔티티이므로 id 필드를 비워둠.
+            return purposeDay;
+        }
+        else {
+            purposeDay.setId(purposeDayInfo.getId());
+            return purposeDay;
+        }
+    }
+}
--- UOSense-Backend/src/main/java/UOSense/UOSense_Backend/entity/PurposeDay.java
+++ UOSense-Backend/src/main/java/UOSense/UOSense_Backend/entity/PurposeDay.java
@@ -25,11 +25,10 @@ public class PurposeDay {
 
     @Column(name = "day_of_week", nullable = false)
     @Enumerated(EnumType.STRING)
-    private DayOfWeek dayOfWeek;
-    public enum DayOfWeek {Monday, Tuesday, Wednesday, Thursday, Friday, Saturday, Sunday}
+    private BusinessDay.DayOfWeek dayOfWeek;
 
     @Column(name = "have_break_time", nullable = false)
-    private boolean haveBreakTime;
+    private boolean breakTime;
 
     @Column(name = "start_break_time")
     private LocalTime startBreakTime;
@@ -44,9 +43,13 @@ public class PurposeDay {
     private LocalTime closingTime;
 
     @Column(name = "is_holiday", nullable = false)
-    private boolean isHoliday;
+    private boolean holiday;
 
     @ManyToOne(fetch = FetchType.LAZY)  // User와 다대일 관계
     @JoinColumn(name = "user_id", nullable = false)  // 외래키 명시
     private User user;
+
+    public void setId(int id) {
+        this.id = id;
+    }
 }
