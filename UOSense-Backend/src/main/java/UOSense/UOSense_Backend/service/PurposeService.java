c249af5fc41293039c4b445409a25dc6e9859757
Gomdadi <35wnsgh@naver.com>
2024-12-13 01:50:22 +0900

    feat: PurposeService findAll, find 메서드 정의. - convertToListDTO 메서드 정의

--- UOSense-Backend/src/main/java/UOSense/UOSense_Backend/service/PurposeService.java
+++ UOSense-Backend/src/main/java/UOSense/UOSense_Backend/service/PurposeService.java
@@ -1,5 +1,12 @@
 package UOSense.UOSense_Backend.service;
 
+import UOSense.UOSense_Backend.dto.PurposeRestListResponse;
+import UOSense.UOSense_Backend.dto.PurposeRestResponse;
+
+import java.util.List;
+
 public interface PurposeService {
     void delete(int restaurantId);
+    List<PurposeRestListResponse> findAll();
+    PurposeRestResponse find(int restaurantId);
 }
--- UOSense-Backend/src/main/java/UOSense/UOSense_Backend/service/PurposeServiceImpl.java
+++ UOSense-Backend/src/main/java/UOSense/UOSense_Backend/service/PurposeServiceImpl.java
@@ -1,6 +1,10 @@
 package UOSense.UOSense_Backend.service;
 
 import UOSense.UOSense_Backend.common.Utils.ImageUtils;
+import UOSense.UOSense_Backend.dto.PurposeRestListResponse;
+import UOSense.UOSense_Backend.dto.PurposeRestResponse;
+import UOSense.UOSense_Backend.entity.PurposeRest;
+import UOSense.UOSense_Backend.entity.PurposeRestImg;
 import UOSense.UOSense_Backend.repository.PurposeMenuRepository;
 import UOSense.UOSense_Backend.repository.PurposeRestImgRepository;
 import UOSense.UOSense_Backend.repository.PurposeRestRepository;
@@ -9,6 +13,9 @@ import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
 
 import java.util.List;
+import java.util.Map;
+import java.util.NoSuchElementException;
+import java.util.stream.Collectors;
 
 @Service
 @RequiredArgsConstructor
@@ -31,5 +38,36 @@ public class PurposeServiceImpl implements PurposeService {
         menuImages.forEach(imageUtils::deleteImageInS3);
 
         purposeRestRepository.deleteById(restaurantId);
+    }
+
+    @Override
+    public List<PurposeRestListResponse> findAll() {
+        List<PurposeRest> purposeRests = purposeRestRepository.findAll();
+        if (purposeRests.isEmpty()) throw new NoSuchElementException("식당 리스트가 존재하지 않습니다.");
+        return convertToListDTO(purposeRests);
+    }
+
+    @Override
+    public PurposeRestResponse find(int restaurantId) {
+        PurposeRest purposeRest = purposeRestRepository.findById(restaurantId)
+                .orElseThrow(() -> new IllegalArgumentException("해당 식당 제안이 존재하지 않습니다."));
+        return PurposeRestResponse.from(purposeRest);
+    }
+
+    private List<PurposeRestListResponse> convertToListDTO(List<PurposeRest> purposeRests) {
+        List<Integer> purposeRestIds = purposeRests.stream().map(PurposeRest::getId).toList();
+        // restaurantId -> imageUrl 매핑 생성
+        Map<Integer, String> imageList = purposeRestImgRepository.findAllFirstImageUrl(purposeRestIds)
+                .stream().collect(Collectors.toMap(
+                        image -> image.getPurposeRest().getId(), // Key: restaurantId
+                        PurposeRestImg::getImageUrl            // Value: imageUrl
+                ));
+        // DTO 변환
+        return purposeRests.stream()
+                .map(purposeRest -> {
+                    String imageUrl = imageList.getOrDefault(purposeRest.getId(), null);
+                    return PurposeRestListResponse.from(purposeRest, imageUrl);
+                })
+                .toList();
     }
 }
