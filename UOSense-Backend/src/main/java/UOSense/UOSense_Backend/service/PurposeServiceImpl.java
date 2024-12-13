0b9ed681d794cc6b9f66acae8a876e20203305a9
Sini0206 <a01092439672@gmail.com>
2024-12-13 11:24:46 +0900

    feat(PurposeServiceImpl): PurposeRestaurant 엔티티를 가져오는 메서드 정의

---
+++ UOSense-Backend/src/main/java/UOSense/UOSense_Backend/service/PurposeServiceImpl.java
@@ -0,0 +1,21 @@
+package UOSense.UOSense_Backend.service;
+
+import UOSense.UOSense_Backend.entity.PurposeRestaurant;
+import UOSense.UOSense_Backend.repository.PurposeRestRepository;
+import lombok.RequiredArgsConstructor;
+import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
+
+@Service
+@RequiredArgsConstructor
+@Transactional(readOnly = true)
+public class PurposeServiceImpl implements  PurposeService{
+    private final PurposeRestRepository purposeRestRepository;
+
+    @Override
+    public PurposeRestaurant getPurposeRestById(int restaurantId) {
+        return purposeRestRepository.findById(restaurantId)
+                .orElseThrow(() -> new IllegalArgumentException("정보 수정 제안 이력이 존재하지 않습니다."));
+    }
+
+}
