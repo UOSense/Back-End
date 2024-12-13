81e5cbba6b2fcde6682b13a7121ecb6b93b27eaf
Gomdadi <35wnsgh@naver.com>
2024-12-13 17:50:36 +0900

    refactor: findByEmail 메서드 User -> Optional<User>로 반환값 변경

--- UOSense-Backend/src/main/java/UOSense/UOSense_Backend/repository/UserRepository.java
+++ UOSense-Backend/src/main/java/UOSense/UOSense_Backend/repository/UserRepository.java
@@ -4,6 +4,8 @@ import UOSense.UOSense_Backend.entity.User;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
+import java.util.Optional;
+
 @Repository
 public interface UserRepository extends JpaRepository<User, Integer>{
     /**
@@ -11,7 +13,7 @@ public interface UserRepository extends JpaRepository<User, Integer>{
      */
     Boolean existsByEmail(String email);
 
-    User findByEmail(String email);
+    Optional<User> findByEmail(String email);
     /**
      * 해당 닉네임으로 가입된 유저가 있는지 확인합니다.
      */
