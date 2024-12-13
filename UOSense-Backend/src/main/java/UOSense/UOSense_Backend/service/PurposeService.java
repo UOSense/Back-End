56e5fa8327cfa8943b018b2594a02026d61f35db
Gomdadi <35wnsgh@naver.com>
2024-12-14 00:57:31 +0900

    feat: 영업 정보 제안, 식당 정보 제안 서비스 클래스

--- UOSense-Backend/src/main/java/UOSense/UOSense_Backend/service/PurposeService.java
+++ UOSense-Backend/src/main/java/UOSense/UOSense_Backend/service/PurposeService.java
@@ -5,10 +5,7 @@ import UOSense.UOSense_Backend.dto.*;
 import java.util.List;
 
 public interface PurposeService {
-    void delete(int restaurantId);
-    List<PurposeRestListResponse> findAll();
-    PurposeRestResponse find(int purposeRestId);
-    PurposeDayList findPurposeDay(int purposeRestId);
-    int register(PurposeRestRequest request, int userId);
-    void registerPurposeDay(PurposeDayList purposeDayList);
+    void register(PurposeRestRequest request, int userId);
+    List<PurposeRestResponse> findList(int restaurantId);
+    void delete(int purposeRestId);
 }
--- UOSense-Backend/src/main/java/UOSense/UOSense_Backend/service/PurposeServiceImpl.java
+++ UOSense-Backend/src/main/java/UOSense/UOSense_Backend/service/PurposeServiceImpl.java
@@ -9,111 +9,53 @@ import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
 
 import java.util.List;
-import java.util.Map;
-import java.util.NoSuchElementException;
 import java.util.Optional;
-import java.util.stream.Collectors;
 
 @Service
 @RequiredArgsConstructor
 @Transactional(readOnly = true)
 public class PurposeServiceImpl implements PurposeService {
-    private final PurposeRestRepository purposeRestRepository;
+    private final PurposeRestaurantRepository purposeRestaurantRepository;
     private final PurposeRestImgRepository purposeRestImgRepository;
-    private final PurposeMenuRepository purposeMenuRepository;
     private final ImageUtils imageUtils;
-    private final PurposeDayRepository purposeDayRepository;
     private final UserRepository userRepository;
+    private final RestaurantRepository restaurantRepository;
 
     @Override
     @Transactional
     public void delete(int purposeRestId) {
-        if (!purposeRestRepository.existsById(purposeRestId)) {
-            throw new IllegalArgumentException("존재하지 않는 식당 정보입니다");
+        if (!purposeRestaurantRepository.existsById(purposeRestId)) {
+            throw new IllegalArgumentException("존재하지 않는 식당 제안 정보입니다");
         }
         List<String> restaurantImages = purposeRestImgRepository.findImageUrls(purposeRestId);
-        List<String> menuImages = purposeMenuRepository.findImageUrls(purposeRestId);
-
-        restaurantImages.forEach(imageUtils::deleteImageInS3);
-        menuImages.forEach(imageUtils::deleteImageInS3);
-
-        purposeRestRepository.deleteById(purposeRestId);
-    }
-
-    @Override
-    public List<PurposeRestListResponse> findAll() {
-        List<PurposeRest> purposeRests = purposeRestRepository.findAll();
-        if (purposeRests.isEmpty()) throw new NoSuchElementException("식당 리스트가 존재하지 않습니다.");
-        return convertToListDTO(purposeRests);
-    }
-
-    @Override
-    public PurposeRestResponse find(int purposeRestId) {
-        PurposeRest purposeRest = purposeRestRepository.findById(purposeRestId)
-                .orElseThrow(() -> new IllegalArgumentException("해당 식당 제안이 존재하지 않습니다."));
-        return PurposeRestResponse.from(purposeRest);
-    }
-
-    private List<PurposeRestListResponse> convertToListDTO(List<PurposeRest> purposeRests) {
-        List<Integer> purposeRestIds = purposeRests.stream().map(PurposeRest::getId).toList();
-        // purposeRestId -> imageUrl 매핑 생성
-        Map<Integer, String> imageList = purposeRestImgRepository.findAllFirstImageUrl(purposeRestIds)
-                .stream().collect(Collectors.toMap(
-                        image -> image.getPurposeRest().getId(), // Key: purposeRestId
-                        PurposeRestImg::getImageUrl            // Value: imageUrl
-                ));
-        // DTO 변환
-        return purposeRests.stream()
-                .map(purposeRest -> {
-                    String imageUrl = imageList.getOrDefault(purposeRest.getId(), null);
-                    return PurposeRestListResponse.from(purposeRest, imageUrl);
-                })
-                .toList();
-    }
-
-    @Override
-    public PurposeDayList findPurposeDay(int purposeRestId) {
-        List<PurposeDay> response = purposeDayRepository.findAllByRestaurantId(purposeRestId);
-        if (response.isEmpty()) {
-            throw new IllegalArgumentException("식당 제안에 대한 영업 정보가 존재하지 않습니다.");
+        if (!restaurantImages.isEmpty()) {
+            restaurantImages.forEach(imageUtils::deleteImageInS3);
         }
-        List<PurposeDayInfo> infoList = response.stream()
-                .map(PurposeDayInfo::from)
-                .toList();
-        return new PurposeDayList(purposeRestId, infoList);
+        purposeRestaurantRepository.deleteById(purposeRestId);
     }
 
+    @Override
+    public List<PurposeRestResponse> findList(int restaurantId) {
+       List<PurposeRestaurant> purposeRestaurant = purposeRestaurantRepository.findAllByRestaurantId(restaurantId);
+       if (purposeRestaurant.isEmpty()) {
+           throw new IllegalArgumentException("해당 식당 제안이 존재하지 않습니다.");
+       }
+       return purposeRestaurant.stream()
+               .map(PurposeRestResponse::from)
+               .toList();
+    }
     @Override
     @Transactional
-    public int register(PurposeRestRequest request, int userId) {
+    public void register(PurposeRestRequest request, int userId) {
         Optional<User> user = userRepository.findById(userId);
         if (user.isEmpty()) {
             throw new IllegalArgumentException("사용자 정보가 없습니다.");
         }
-        PurposeRest purposeRest = PurposeRestRequest.toEntity(request, user.get());
-        return purposeRestRepository.save(purposeRest).getId();
-    }
-
-    @Override
-    @Transactional
-    public void registerPurposeDay(PurposeDayList purposeDayList) {
-        PurposeRest purposeRest = purposeRestRepository.findById(purposeDayList.getPurposeRestId())
-                .orElseThrow(() -> new NoSuchElementException("존재하지 않는 식당 제안입니다."));
-
-        List<PurposeDayInfo> InfoList = purposeDayList.getPurposeDayInfoList();
-        for(PurposeDayInfo purposeDayInfo : InfoList) {
-            // breakTime이 없을 경우
-            if (!purposeDayInfo.isBreakTime()) {
-                purposeDayInfo.setBreakTime();
-            }
-
-            // 휴무일일 경우
-            if (purposeDayInfo.isHoliday()) {
-                purposeDayInfo.setTime();
-            }
-
-            PurposeDay purposeDay = PurposeDayInfo.toEntity(purposeDayInfo, purposeRest);
-            purposeDayRepository.save(purposeDay);
+        Optional<Restaurant> restaurant = restaurantRepository.findById(request.getRestaurantId());
+        if (restaurant.isEmpty()) {
+            throw new IllegalArgumentException("식당 정보가 없습니다.");
         }
+        PurposeRestaurant purposeRestaurant = PurposeRestRequest.toEntity(request, restaurant.get(), user.get());
+        purposeRestaurantRepository.save(purposeRestaurant);
     }
 }
