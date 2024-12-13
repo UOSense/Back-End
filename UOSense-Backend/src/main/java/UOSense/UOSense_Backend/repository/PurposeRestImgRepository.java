0e2d856b6bdb7d0dc4e926d70d997aa5220499ff
Gomdadi <35wnsgh@naver.com>
2024-12-14 00:58:42 +0900

    refactor: 불필요한 import 문 제거

--- UOSense-Backend/src/main/java/UOSense/UOSense_Backend/repository/PurposeRestImgRepository.java
+++ UOSense-Backend/src/main/java/UOSense/UOSense_Backend/repository/PurposeRestImgRepository.java
@@ -2,7 +2,6 @@ package UOSense.UOSense_Backend.repository;
 
 import UOSense.UOSense_Backend.entity.PurposeRestImg;
 import org.springframework.data.jpa.repository.JpaRepository;
-import org.springframework.data.jpa.repository.Modifying;
 import org.springframework.data.jpa.repository.Query;
 import org.springframework.data.repository.query.Param;
 import org.springframework.stereotype.Repository;
